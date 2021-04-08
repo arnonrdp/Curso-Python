33509641924f9d6225a08fa684de51e716fc00a0
Arnon Rodrigues <arnonrdp@gmail.com>
2021-04-08 13:51:00 -0300

    Exercicio 004

---
+++ ex004.py
@@ -0,0 +1,8 @@
+n = input('Digite algo: ')
+print('O tipo primitivo desse valor é: ', type(n))
+print('É alfabético?', n.isalpha())
+print('É numérico?', n.isnumeric())
+print('É alfanumérico??', n.isalnum())
+print('Está em maiúsculas?', n.isupper())
+print('Está em minúsculas?', n.islower())
+print('Está capitalizada?', n.istitle())
