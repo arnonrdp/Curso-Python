9103b47fd9a34f2f50161eb886f712b254cc0e90
Arnon Rodrigues <arnonrdp@gmail.com>
2021-04-26 13:51:00 -0300

    Exercicio 025

---
+++ ex025.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite o nome do malandro: ')).strip()
+
+if 'silva' in nome.lower():
+    print('Esse cabra tem Silva no nome.')
+else:
+    print('Esse cabra NÃO tem Silva no nome')
