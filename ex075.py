db8e6ef9d0f3d68fa528ea89d80c656fd3c68555
Arnon Rodrigues <arnonrdp@gmail.com>
2021-07-05 13:51:00 -0300

    Exercicio 075

---
+++ ex075.py
@@ -0,0 +1,22 @@
+"""
+Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla.
+No final, mostre:
+A) Quantas vezes apareceu o valor 9.
+B) Em que posição foi digitado o primeiro valor 3.
+C) Quais foram os números pares.
+"""
+tupla = (int(input('Digite um número: ')),
+         int(input('Digite outro número: ')),
+         int(input('Digite mais um número: ')),
+         int(input('Digite o último número: ')))
+print(f'O valor 9 apareceu {tupla.count(9)} vez(es)')
+
+if 3 in tupla:
+    print(f'O valor 3 apareceu na {tupla.index(3)+1}ª posição.')
+else:
+    print('O valor 3 não foi digitado em nenhuma posição.')
+
+print(f'Os valores pares digitados foram:', end=' ')
+for par in tupla:
+    if par % 2 == 0:
+        print(par, end=' ')
