349a04f6ed2b5f1ecc8a0a9390dfbee31287207e
Arnon Rodrigues <arnonrdp@gmail.com>
2021-10-15 13:51:00 -0300

    Exercicio 107

---
+++ ex107/moeda.py
@@ -0,0 +1,19 @@
+def metade(n):
+    return n / 2
+
+
+def dobro(n):
+    return n * 2
+
+
+def aumentar(n, v=0):
+    return n + n * v / 100
+
+
+def diminuir(n, v=0):
+    return n - n * v / 100
+
+"""
+Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade().
+Faça também um programa que importe esse módulo e use algumas dessas funções.
+"""
